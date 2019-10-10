7bf0b114c61bfb1df2ac49e66b3f6da0ca22bd9d
Aaron Wyllie <aaron.t.wyllie@gmail.com>
2019-10-09 21:35:52 -0400

    Update

--- manifests/file_hello.pp
+++ manifests/file_hello.pp
@@ -1,5 +1,4 @@
 file { '/tmp/hello.txt':
   ensure  => file,
-  content => "# This file is managed by Puppet - any manual edits will be lost.\n# HEADER: While it can still be managed manually, it is definitely not
-#HEADER: recommended. Note particularly that the comments starting with 'Puppet Name' should\n# HEADER: not be deleted, as doing so could cause duplicate cron jobs.\n# Puppet Name: run-puppet\n*/15 * * * * /usr/local/bin/run-puppet\n",
+  content => "# This file is managed by Puppet - any manual edits will be lost.\n# HEADER: While it can still be managed manually, it is definitely not recommended.\n# HEADER: Note particularly that the comments starting with 'Puppet Name' should\n# HEADER: not be deleted, as doing so could cause duplicate cron jobs.\n# Puppet Name: run-puppet\n*/15 * * * * /usr/local/bin/run-puppet\n",
 }
